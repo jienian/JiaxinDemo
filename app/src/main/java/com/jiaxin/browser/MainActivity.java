30a5981e7b74627d91cef7f279abbeac5f3d0ace
jienian <2129300193@qq.com>
2023-12-18 18:47:32 +0800

    实现百度浏览器的Demo

--- app/src/main/java/com/jiaxin/browser/MainActivity.java
+++ app/src/main/java/com/jiaxin/browser/MainActivity.java
@@ -2,13 +2,43 @@ package com.jiaxin.browser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private Button searchButton;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        searchButton = (Button) findViewById(R.id.button_search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.setVisibility(View.VISIBLE);
+                webView.loadUrl("https://www.baidu.com");
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.getVisibility() == View.VISIBLE) {
+            webView.setVisibility(View.GONE);
+            searchButton.setVisibility(View.VISIBLE);
+        } else {
+            super.onBackPressed();//退出App
+        }
     }
 }
