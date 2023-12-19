bd08e323a38bea18ebf3c90a2752a0920b75ed4c
jienian <2129300193@qq.com>
2023-12-19 14:21:42 +0800

    [develop] 兼容鸿蒙生态和用JS截取下载

--- app/src/main/java/com/jiaxin/browser/MainActivity.java
+++ app/src/main/java/com/jiaxin/browser/MainActivity.java
@@ -1,17 +1,28 @@
 package com.jiaxin.browser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.JavascriptInterface;
+import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jiaxin.browser.service.AdService;
 
@@ -24,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Intent adServiceIntent = new Intent(this, AdService.class);
         startService(adServiceIntent);
@@ -38,11 +50,39 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
 
-
-
         webView = (WebView) findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        //js截取
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:(function() { " +
+                        " var links = document.getElementsByTagName('a'); " +
+                        " for(var i = 0; i < links.length; i++) {" +
+                        "   if(links[i].href.includes('url')) {" +
+                        "     links[i].onclick = function() {" +
+                        "       Android.downloadFile(this.href); " +
+                        "       return false; " +
+                        "     }" +
+                        "   }" +
+                        " }" +
+                        "})()");
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("WebView", consoleMessage.message() + " -- From line "
+                        + consoleMessage.lineNumber() + " of "
+                        + consoleMessage.sourceId());
+                return super.onConsoleMessage(consoleMessage);
+            }
+        });
 
         searchButton = (Button) findViewById(R.id.button_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
@@ -53,13 +93,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void downloadFile(String url) {
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+            String cookies = CookieManager.getInstance().getCookie(url);
+            request.addRequestHeader("cookie", cookies);
+            request.addRequestHeader("User-Agent", webView.getSettings().getUserAgentString());
+            request.setDescription("下载文件中...");
+            request.setTitle(URLUtil.guessFileName(url, null, null));
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, null, null));
+
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            dm.enqueue(request);
+            Toast.makeText(getApplicationContext(), "下载文件", Toast.LENGTH_LONG).show();
+        }
+    }
+
     @Override
     public void onBackPressed() {
         if (webView.getVisibility() == View.VISIBLE) {
             webView.setVisibility(View.GONE);
             searchButton.setVisibility(View.VISIBLE);
         } else {
-            super.onBackPressed();//退出App
+            super.onBackPressed(); // 退出App
         }
     }
 }
